19c4a5ae93dba85701ae6413110d012754b9efce
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-03-21 18:49:48 +0100

    more sensible defaults for lualine

--- lua/plugins/status-line.lua
+++ lua/plugins/status-line.lua
@@ -6,8 +6,8 @@ return {
 			options = {
 				icons_enabled = true,
 				theme = "auto",
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
+				component_separators = "|",
+				section_separators = "",
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},
@@ -23,11 +23,11 @@ return {
 			},
 			sections = {
 				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_b = { "branch", "diff" },
 				lualine_c = { "filename" },
-				lualine_x = { "encoding", "fileformat", "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
+				lualine_x = { "diagnostics" },
+				lualine_y = { "filetype" },
+				lualine_z = {},
 			},
 			inactive_sections = {
 				lualine_a = {},
