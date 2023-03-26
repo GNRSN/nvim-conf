30c7326cd5a29f3ce0c43e1b6760a5fb8359c09e
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-03-26 15:59:00 +0200

    move neovide specifict into separate file

--- init.lua
+++ init.lua
@@ -38,19 +38,5 @@ else
 end
 
 if vim.g.neovide then
-	vim.o.guifont = "Hack_Nerd_Font"
-	vim.g.neovide_cursor_animate_in_insert_mode = false
-  vim.g.neovide_cursor_vfx_mode = "pixiedust"
-	
-		-- Helper function for transparency formatting
-	local get_alpha = function()
-		return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
-	end
-	-- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-	vim.g.neovide_transparency = 0.0
-	vim.g.transparency = 0.8
-	vim.g.neovide_background_color = "#282A36" .. get_alpha()
-	vim.g.neovide_floating_blur_amount_x = 4.0
-	vim.g.neovide_floating_blur_amount_y = 4.0
-	
+	require('neovide')
 end
---
+++ lua/neovide/init.lua
@@ -0,0 +1,52 @@
+
+
+if not vim.g.neovide then
+  error('Neovide customization initilized but neovide env is not present')
+end
+
+-- set font
+vim.o.guifont = "Hack_Nerd_Font:h13"
+
+-- set animations
+vim.g.neovide_cursor_animate_in_insert_mode = false
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
+
+-- set styling
+-- Helper function for transparency formatting
+local get_alpha = function()
+  return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+end
+-- DOCS: g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+vim.g.neovide_transparency = 0.0
+vim.g.transparency = 0.90
+vim.g.neovide_background_color = "#282A36" .. get_alpha()
+vim.g.neovide_floating_blur_amount_x = 4.0
+vim.g.neovide_floating_blur_amount_y = 4.0
+
+-- set scaling and add keymaps for updating
+vim.g.neovide_scale_factor = 1.0
+local change_scale_factor = function(delta)
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+end
+vim.keymap.set("n", "<C-+>", function()
+  change_scale_factor(1.15)
+end)
+vim.keymap.set("n", "<C-->", function()
+  change_scale_factor(1/1.15)
+end)
+
+-- setup copy paste to clipboard with cmd+c
+vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
+vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+
+-- Allow clipboard copy paste in neovim
+vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
