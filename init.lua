c272900166e3cf13b65937a72dc9b81a4280b34e
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-06-11 20:24:32 +0200

    update config to allow usage with VScode

--- init.lua
+++ init.lua
@@ -1,27 +1,25 @@
-
--- bootstrap lazy-loader
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
 if vim.g.vscode then
 	-- VSCode extension
-	print("vim.g.vscode env present, avoids loading plugins")
+	require("vscode")
 else
 	-- ordinary Neovim
+	-- bootstrap lazy-loader
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable", -- latest stable release
+			lazypath,
+		})
+	end
+	vim.opt.rtp:prepend(lazypath)
 
 	require("codex").setup({
 		transparent_bg = true,
-  })
+	})
 
 	local colorscheme = "codex"
 
@@ -32,11 +30,11 @@ else
 	end
 
 	-- use lazy-loader
-  require("lazy").setup("plugins")
+	require("lazy").setup("plugins")
 	require("config").setup()
 	require("after")
-end
 
-if vim.g.neovide then
-	require('neovide')
+	if vim.g.neovide then
+		require("neovide")
+	end
 end
