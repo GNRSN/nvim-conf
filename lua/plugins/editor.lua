258372f246277a99faed11c75b23a69d73aaa633
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-07-14 14:07:32 +0200

    fix hapoon keybinds & window width

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -152,6 +152,7 @@ return {
 				["<leader>u"] = { name = "+ui" },
 				["<leader>w"] = { name = "+windows" },
 				["<leader>x"] = { name = "+diagnostics/quickfix" },
+				["<leader>h"] = { name = "+Harpoon" },
 			}
 			if Util.has("noice.nvim") then
 				keymaps["<leader>sn"] = { name = "+noice" }
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,13 +8,17 @@ return {
 		config = function()
 			local harpoon = require("harpoon")
 
-			harpoon.setup({})
+			harpoon.setup({
+				menu = {
+					width = vim.api.nvim_win_get_width(0) - 4,
+				},
+			})
 
 			nmap("<leader>hm", require("harpoon.mark").add_file, "Harpoon mark")
 			nmap("<leader>hl", require("harpoon.ui").toggle_quick_menu, "Harpoon toggle menu")
 
 			for i = 1, 5 do
-				nmap(string.format("g%s", i), function()
+				nmap(string.format("<leader>%s", i), function()
 					require("harpoon.ui").nav_file(i)
 				end, { desc = string.format("Navigate to harpoon (%s)", i) })
 			end
