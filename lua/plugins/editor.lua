060eeae1efbc70069e55b79c4b80fbb49ff855fe
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-19 22:19:18 +0100

    remove indent-blankline

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -339,16 +339,6 @@ return {
 	},
 
 	{
-		"lukas-reineke/indent-blankline.nvim",
-		opts = {
-			show_current_context = true,
-			show_current_context_start = true,
-			use_treesitter = true,
-			use_treesitter_scope = true,
-		},
-	},
-
-	{
 		"windwp/nvim-autopairs",
 		opts = {
 			disable_filetype = { "TelescopePrompt", "vim" },
