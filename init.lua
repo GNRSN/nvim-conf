1eb11b18c8491a3993d96440b00eb2f021d7120d
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-22 21:15:23 +0100

    formatting

--- init.lua
+++ init.lua
@@ -1,46 +1,45 @@
 if vim.g.vscode then
-	-- VSCode extension
-	require("vscode")
+  -- VSCode extension
+  require("vscode")
 else
-	-- ordinary Neovim
-	-- bootstrap lazy-loader
-	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-	if not vim.loop.fs_stat(lazypath) then
-		vim.fn.system({
-			"git",
-			"clone",
-			"--filter=blob:none",
-			"https://github.com/folke/lazy.nvim.git",
-			"--branch=stable", -- latest stable release
-			lazypath,
-		})
-	end
-	vim.opt.rtp:prepend(lazypath)
+  -- ordinary Neovim
+  -- bootstrap lazy-loader
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
 
-	require("colorscheme.init").setup({
-		transparent_bg = true,
-	})
-	require("colorscheme.init").load()
+  require("colorscheme").setup({
+    transparent_bg = true,
+  })
+  require("colorscheme").load()
 
+  -- use lazy-loader
+  require("lazy").setup({
+    {
+      import = "plugins",
+    },
+    {
+      import = "plugins.lsp",
+    },
+    {
+      import = "plugins.treesitter",
+    },
+    {
+      -- options for lazy can go here, check out what's available later
+    },
+  })
+  require("config").setup()
 
-	-- use lazy-loader
-	require("lazy").setup({
-		{
-			import = "plugins",
-		},
-		{
-			import = "plugins.lsp",
-		},
-		{
-			import = "plugins.treesitter",
-		},
-        {
-	-- options for lazy can go here, check out what's available later		
-		}
-	})
-	require("config").setup()
-
-	if vim.g.neovide then
-		require("neovide")
-	end
+  if vim.g.neovide then
+    require("neovide")
+  end
 end
