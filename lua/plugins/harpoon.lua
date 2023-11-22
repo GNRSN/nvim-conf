ca3ab4e7b353a707b73f89b11e24ff7633797a6a
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-22 21:00:07 +0100

    restore harpoon config

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,15 +4,18 @@ return {
     "nvim-lua/plenary.nvim",
   },
   config = function()
+    local harpoon = require("harpoon")
+
+    harpoon.setup({
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      },
+    })
+
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set(
-      "n",
-      "<leader>hm",
-      "<cmd>lua require('harpoon.mark').add_file()<cr>",
-      { desc = "Mark file with harpoon" }
-    )
+    keymap.set("n", "<leader>hm", require("harpoon.mark").add_file, { desc = "Harpoon mark" })
     keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
     keymap.set(
       "n",
@@ -20,5 +23,13 @@ return {
       "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
       { desc = "Go to previous harpoon mark" }
     )
+
+    keymap.set("n", "<leader>hl", require("harpoon.ui").toggle_quick_menu, "Harpoon toggle list")
+
+    for i = 1, 9 do
+      keymap.set("n", string.format("<leader>%s", i), function()
+        require("harpoon.ui").nav_file(i)
+      end, { desc = string.format("Navigate to harpoon (%s)", i) })
+    end
   end,
 }
