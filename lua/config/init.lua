fc96320a69a5c1144af95f97a379903f195f7bf7
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-23 15:08:06 +0100

    formatting

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,20 +1,20 @@
 local function load(name)
-	local Util = require("lazy.core.util")
-	local function _load(mod)
-		Util.try(function()
-			require(mod)
-		end, {
-			msg = "Failed loading " .. mod,
-			on_error = function(msg)
-				local modpath = require("lazy.core.cache").find(mod)
-				if modpath then
-					Util.error(msg)
-				end
-			end,
-		})
-	end
+  local Util = require("lazy.core.util")
+  local function _load(mod)
+    Util.try(function()
+      require(mod)
+    end, {
+      msg = "Failed loading " .. mod,
+      on_error = function(msg)
+        local modpath = require("lazy.core.cache").find(mod)
+        if modpath then
+          Util.error(msg)
+        end
+      end,
+    })
+  end
 
-	_load("config." .. name)
+  _load("config." .. name)
 end
 
 -- REVIEW: Wtf is all this for?
@@ -57,17 +57,17 @@ local function lazy_notify()
 end
 
 return {
-	init = function()
-		-- delay notifications till vim.notify was replaced or after 500ms
-		lazy_notify()
+  init = function()
+    -- delay notifications till vim.notify was replaced or after 500ms
+    lazy_notify()
 
-		-- load options here, before lazy init while sourcing plugin modules
-		-- this is needed to make sure options will be correctly applied
-		-- after installing missing plugins
-		load("options")
-	end,
-	setup = function(opts)
-		load("autocmds")
-		load("keymaps")
-	end,
+    -- load options here, before lazy init while sourcing plugin modules
+    -- this is needed to make sure options will be correctly applied
+    -- after installing missing plugins
+    load("options")
+  end,
+  setup = function(opts)
+    load("autocmds")
+    load("keymaps")
+  end,
 }
