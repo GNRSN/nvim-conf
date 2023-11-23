358313dbc8a5919a8c932b16de92800406c53d86
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-23 17:44:55 +0100

    move disabled lspsaga to own file

---
+++ lua/plugins/ui/lspsaga.lua
@@ -0,0 +1,32 @@
+-- TODO: See if I stil want this or if some other UI is good enough
+return {
+  -- {
+  -- 	"glepnir/lspsaga.nvim",
+  -- 	event = "BufRead",
+  -- 	config = function()
+  -- 		require("lspsaga").setup({
+  -- 			lightbulb = {
+  -- 				enable = false,
+  -- 			},
+  -- 			code_action = {
+  -- 				show_server_name = true,
+  -- 				extend_gitsigns = true,
+  -- 			},
+  -- 			rename = {
+  -- 				quit = "q",
+  -- 			},
+  -- 			ui = {
+  -- 				title = false,
+  -- 			},
+  -- 			symbol_in_winbar = {
+  -- 				enable = false,
+  -- 			},
+  -- 		})
+  -- 	end,
+  -- 	dependencies = {
+  -- 		{ "nvim-tree/nvim-web-devicons" },
+  -- 		--Please make sure you install markdown and markdown_inline parser
+  -- 		{ "nvim-treesitter/nvim-treesitter" },
+  -- 	},
+  -- },
+}
