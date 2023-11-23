e4509b74496d5ae33ece05e003f1cfb097d453be
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-23 18:17:07 +0100

    setup todo comments with colors and keybinds

--- lua/plugins/navigation/todo-comments.lua
+++ lua/plugins/navigation/todo-comments.lua
@@ -8,27 +8,26 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     opts = {
       keywords = {
-        -- TODO: Set colors
         REVIEW = { icon = " ", color = "review" },
         PERF = { icon = " ", color = "perf" },
-        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-        TODO = { icon = " ", color = "info" },
+        NOTE = { icon = " ", color = "note", alt = { "INFO" } },
+        TODO = { icon = " ", color = "todo" },
       },
       merge_keywords = false,
       colors = {
         review = { palette.comment },
-        perf = { palette.yellow },
-        hint = { palette.blue },
-        info = { palette.white },
+        perf = { palette.purple },
+        note = { palette.white },
+        todo = { palette.yellow_sunflower },
       },
     },
     -- stylua: ignore
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-      { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
-      { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+      { "<leader>xt", "<cmd>TodoTrouble keywords=TODO<cr>", desc = "Todo(s) (Trouble)" },
+      { "<leader>xr", "<cmd>TodoTrouble keywords=REVIEW,PERF<cr>", desc = "Review & Perf (Trouble)" },
+      { "<leader>ft", "<cmd>TodoTelescope keywords=TODO<cr>", desc = "Todo" },
     },
   },
 }
