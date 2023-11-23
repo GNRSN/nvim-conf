e9c9164a5f8ab22572639fa508c34364a46afaac
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-23 18:02:53 +0100

    update keybind for git status

--- lua/plugins/navigation/telescope.lua
+++ lua/plugins/navigation/telescope.lua
@@ -25,8 +25,8 @@ return {
       { "<leader>F", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
       { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
       -- git
+      { "<leader>fg", "<cmd>Telescope git_status<CR>", desc = "status" },
       { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
-      { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "status" },
       -- search
       { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
       { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
