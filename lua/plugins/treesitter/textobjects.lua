f7a48eea2b6e95dee9cbbbf17eada7fa90a78eb4
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-24 15:41:35 +0100

    add comments

--- lua/plugins/treesitter/textobjects.lua
+++ lua/plugins/treesitter/textobjects.lua
@@ -1,8 +1,8 @@
 return {
+  -- Allows parsing and manipulation of textobjects, smarter than just words,
+  -- Other extenions may improve their behavior with this enabled
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
-    lazy = true,
-    after = "nvim-treesitter",
     dependencies = "nvim-treesitter/nvim-treesitter",
   },
 }
