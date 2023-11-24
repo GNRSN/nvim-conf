d9bd619bc519af79a8982cd182a56d7e709b8ce5
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-24 13:35:13 +0100

    add yanky

---
+++ lua/plugins/editing/yanky.lua
@@ -0,0 +1,33 @@
+return {
+  -- Better yanking with history, ctrl p/n to replace with previous yank after putting
+  {
+    "gbprod/yanky.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      -- Basic usage
+      { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Yanky put (after)" },
+      { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Yanky put (before)" },
+      { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Yanky G-put (after)" },
+      { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Yanky G-put (before)" },
+      { "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Yanky previous entry" },
+      { "<c-n>", "<Plug>(YankyNextEntry)", desc = "Yanky next entry" },
+      -- Tpope/vim-unimpaired 'esque usage
+      { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Yanky put indent after linewise" },
+      { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Yanky put indent before linewise" },
+      { "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Yanky put indent after linewise" },
+      { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Yanky put indent before linewise" },
+
+      { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Yanky put indent after linewise" },
+      { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Yanky put indent after linewise" },
+      { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Yanky put indent after linewise" },
+      { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Yanky put indent after linewise" },
+
+      { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Yanky put indent after linewise" },
+      { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Yanky put indent after linewise" },
+    },
+  },
+}
