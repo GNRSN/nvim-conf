fbfcaf63ffe9e5faa5baf6c74af43a602f2a5e66
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-25 15:03:52 +0100

    review-comment

--- lua/plugins/ui/status-line.lua
+++ lua/plugins/ui/status-line.lua
@@ -1,59 +1,62 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-    "kyazdani42/nvim-web-devicons",
-    -- Add as dependency to integrate
-    "linrongbin16/lsp-progress.nvim",
-  },
-  config = function()
-    require("lualine").setup({
-      options = {
-        icons_enabled = true,
-        theme = require("colorscheme.status-line"),
-        component_separators = "|",
-        section_separators = "",
-        disabled_filetypes = {
-          statusline = {},
-          winbar = {},
+  -- TODO: Can we avoid adding to file explorer?
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "kyazdani42/nvim-web-devicons",
+      -- Add as dependency to integrate
+      "linrongbin16/lsp-progress.nvim",
+    },
+    config = function()
+      require("lualine").setup({
+        options = {
+          icons_enabled = true,
+          theme = require("colorscheme.status-line"),
+          component_separators = "|",
+          section_separators = "",
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+          },
         },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff" },
+          lualine_c = { "filename" },
+          lualine_x = { require("lsp-progress").progress, "diagnostics" },
+          lualine_y = { "filetype" },
+          lualine_z = {},
         },
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff" },
-        lualine_c = { "filename" },
-        lualine_x = { require("lsp-progress").progress, "diagnostics" },
-        lualine_y = { "filetype" },
-        lualine_z = {},
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      winbar = {},
-      inactive_winbar = {},
-      extensions = {},
-    })
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
+      })
 
-    -- listen lsp-progress event and refresh lualine
-    vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
-    vim.api.nvim_create_autocmd("User", {
-      group = "lualine_augroup",
-      pattern = "LspProgressStatusUpdated",
-      callback = require("lualine").refresh,
-    })
-  end,
+      -- listen lsp-progress event and refresh lualine
+      vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+      vim.api.nvim_create_autocmd("User", {
+        group = "lualine_augroup",
+        pattern = "LspProgressStatusUpdated",
+        callback = require("lualine").refresh,
+      })
+    end,
+  },
 }
