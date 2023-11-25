0906efb5f9f6cedb46134af76bfb1e2513169f59
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-25 18:23:43 +0100

    add local highlights

---
+++ lua/plugins/ui/local-highlights.lua
@@ -0,0 +1,16 @@
+return {
+  -- Regext based highlighting of only what is in view,
+  -- claims to be faster than treesitter in large files
+  -- REVIEW: Is it really as good as treesiter based though? e.g. highlighting doesn't work for words with hyphen
+  -- maybe treesitter version could be more intelligent? I guess it's pretty good that it follows the same logic as w/e/b motions
+  -- I think [https://github.com/RRethy/vim-illuminate] is the alternative, which uses ts textobjects, but is maybe less performant
+  {
+    "tzachar/local-highlight.nvim",
+    config = function()
+      require("local-highlight").setup({
+        hlgroup = "LocalHighlight",
+        cw_hlgroup = "LocalHighlight",
+      })
+    end,
+  },
+}
