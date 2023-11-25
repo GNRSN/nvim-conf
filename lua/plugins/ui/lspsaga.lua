dda81320a0f1331440ef087de14054bc10b59590
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-25 09:35:59 +0100

    enable lsp-saga

--- lua/plugins/ui/lspsaga.lua
+++ lua/plugins/ui/lspsaga.lua
@@ -1,32 +1,35 @@
 -- TODO: See if I stil want this or if some other UI is good enough
 return {
-  -- {
-  -- 	"glepnir/lspsaga.nvim",
-  -- 	event = "BufRead",
-  -- 	config = function()
-  -- 		require("lspsaga").setup({
-  -- 			lightbulb = {
-  -- 				enable = false,
-  -- 			},
-  -- 			code_action = {
-  -- 				show_server_name = true,
-  -- 				extend_gitsigns = true,
-  -- 			},
-  -- 			rename = {
-  -- 				quit = "q",
-  -- 			},
-  -- 			ui = {
-  -- 				title = false,
-  -- 			},
-  -- 			symbol_in_winbar = {
-  -- 				enable = false,
-  -- 			},
-  -- 		})
-  -- 	end,
-  -- 	dependencies = {
-  -- 		{ "nvim-tree/nvim-web-devicons" },
-  -- 		--Please make sure you install markdown and markdown_inline parser
-  -- 		{ "nvim-treesitter/nvim-treesitter" },
-  -- 	},
-  -- },
+  {
+    "nvimdev/lspsaga.nvim",
+    event = "BufRead",
+    config = function()
+      require("lspsaga").setup({
+        lightbulb = {
+          enable = false,
+        },
+        code_action = {
+          show_server_name = true,
+          extend_gitsigns = true,
+        },
+        rename = {
+          quit = "q",
+        },
+        ui = {
+          title = false,
+        },
+        symbol_in_winbar = {
+          enable = false,
+        },
+      })
+    end,
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons" },
+      --Please make sure you install markdown and markdown_inline parser
+      { "nvim-treesitter/nvim-treesitter" },
+    },
+    keys = {
+      { "K", "<cmd>Lspsaga hover_doc<cr>" },
+    },
+  },
 }
