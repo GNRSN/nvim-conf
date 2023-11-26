562c16b0c976e26ae43399d608e15ecc99b18171
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-26 01:03:36 +0100

    comment

--- lua/plugins/ui/local-highlights.lua
+++ lua/plugins/ui/local-highlights.lua
@@ -4,6 +4,11 @@ return {
   -- REVIEW: Is it really as good as treesiter based though? e.g. highlighting doesn't work for words with hyphen
   -- maybe treesitter version could be more intelligent? I guess it's pretty good that it follows the same logic as w/e/b motions
   -- I think [https://github.com/RRethy/vim-illuminate] is the alternative, which uses ts textobjects, but is maybe less performant
+  -- Actually, there's a bunch of them
+  -- - https://github.com/yamatsum/nvim-cursorline
+  -- - https://github.com/xiyaowong/nvim-cursorword
+  -- - https://github.com/sontungexpt/stcursorword
+  -- -
   {
     "tzachar/local-highlight.nvim",
     config = function()
