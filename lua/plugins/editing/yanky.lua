a1358f6ab7a8c507d42ef6a266a6217d1e1e0857
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-26 17:00:36 +0100

    comments

--- lua/plugins/editing/yanky.lua
+++ lua/plugins/editing/yanky.lua
@@ -3,9 +3,8 @@ return {
   {
     "gbprod/yanky.nvim",
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      -- TODO: Make yank/put colors a bit more subtle?
+      -- Also, does cut work?
     },
     keys = {
       -- Basic usage
