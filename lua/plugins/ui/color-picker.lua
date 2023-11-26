16ca81f08e63e472e947b613a34969665dbcbaa6
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-11-26 12:30:16 +0100

    add colorpicker, seems to replace colorizer

---
+++ lua/plugins/ui/color-picker.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "uga-rosa/ccc.nvim",
+    config = function()
+      -- Enable true color
+      vim.opt.termguicolors = true
+
+      local ccc = require("ccc")
+
+      ccc.setup({
+        -- Your preferred settings
+        -- Example: enable highlighter
+        highlighter = {
+          auto_enable = true,
+          lsp = true,
+        },
+        mapping = {
+          -- TODO: This doesn't apply
+          ["<esc>"] = function()
+            ccc.mapping.quit()
+          end,
+        },
+        inputs = {
+          ccc.input.hsl,
+          ccc.input.rgb,
+        },
+      })
+    end,
+    event = { "BufReadPre", "BufNewFile" },
+    keys = {
+      { "<leader>cp", "<cmd>CccPick<cr>", desc = "Open color picker" },
+    },
+  },
+}
