fadd674ce21715ba29c7e3ba6ff01a0d9a042755
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2023-12-28 18:38:52 +0100

    migrate to harpoon 2

--- lua/plugins/navigation/harpoon.lua
+++ lua/plugins/navigation/harpoon.lua
@@ -1,35 +1,48 @@
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
   config = function()
     local harpoon = require("harpoon")
 
-    harpoon.setup({
-      menu = {
-        width = vim.api.nvim_win_get_width(0) - 4,
+    harpoon:setup({
+      settings = {
+        save_on_toggle = true,
       },
     })
 
     local keymap = vim.keymap
     local wk = require("which-key")
 
-    keymap.set("n", "<leader>hm", require("harpoon.mark").add_file, { desc = "Harpoon mark" })
-    keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
-    keymap.set(
-      "n",
-      "<leader>hp",
-      "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
-      { desc = "Go to previous harpoon mark" }
-    )
+    keymap.set("n", "<leader>hm", function()
+      harpoon:list():append()
 
-    keymap.set("n", "<leader>hl", require("harpoon.ui").toggle_quick_menu, { desc = "Harpoon toggle list" })
+      local n_harpoons = 0
+      for _ in pairs(harpoon:list().items) do
+        n_harpoons = n_harpoons + 1
+      end
+
+      vim.notify(string.format("Added file (#%s)", n_harpoons))
+    end, { desc = "Harpoon mark" })
+
+    keymap.set("n", "<leader>hn", function()
+      harpoon:list():prev()
+    end, { desc = "Go to next harpoon mark" })
+
+    keymap.set("n", "<leader>hp", function()
+      harpoon:list():next()
+    end, { desc = "Go to previous harpoon mark" })
+
+    keymap.set("n", "<leader>hl", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = "Harpoon toggle list" })
 
     for i = 1, 9 do
       local key = string.format("<leader>%s", i)
       keymap.set("n", key, function()
-        require("harpoon.ui").nav_file(i)
+        harpoon:list():select(i)
       end, { desc = string.format("Navigate to harpoon (%s)", i) })
 
       -- Hide from which-key
