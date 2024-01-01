4f25a35f0ebca86cc21a092921ca8112faff4517
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2024-01-01 19:16:36 +0100

    set defualt file icon

--- lua/plugins/navigation/neotree.lua
+++ lua/plugins/navigation/neotree.lua
@@ -4,7 +4,7 @@ return {
     branch = "v3.x",
     depencencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
       {
@@ -82,7 +82,7 @@ return {
             folder_empty = "󰉖",
             -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
             -- then these will never be used.
-            default = "*",
+            default = "",
             highlight = "NeoTreeFileIcon",
           },
           modified = {
