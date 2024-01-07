1582ae093f44e5d46968565812c587a6b1cd0f2c
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2024-01-07 14:11:47 +0100

    add more case insensitive commands

--- lua/config/usercmds.lua
+++ lua/config/usercmds.lua
@@ -1,3 +1,16 @@
+-- Because : requires pressing shift on nordic keyboard layout
 vim.api.nvim_create_user_command("Wa", function()
   vim.cmd.wa()
+end, {})
+
+vim.api.nvim_create_user_command("W", function()
+  vim.cmd.W()
+end, {})
+
+vim.api.nvim_create_user_command("Qa", function()
+  vim.cmd.qa()
+end, {})
+
+vim.api.nvim_create_user_command("Q", function()
+  vim.cmd.q()
 end, {})
