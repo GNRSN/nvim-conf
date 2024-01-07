2d981c58ae9a1e1470fea8009dc1eaa589c62881
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2024-01-07 19:57:25 +0100

    remap pound to dollar

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,7 @@ local Util = require("util")
 local function map(mode, lhs, rhs, opts)
   local keys = require("lazy.core.handler").handlers.keys
   -- do not create the keymap if a lazy keys handler exists
+  ---@diagnostic disable-next-line: undefined-field
   if not keys.active[keys.parse({ lhs, mode = mode }).id] then
     opts = opts or {}
     opts.silent = opts.silent ~= false
@@ -107,3 +108,6 @@ map("n", "<leader>gG", function() Util.float_term({ "lazygit" }) end, { desc = "
 
 -- highlights under cursor
 map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
+
+-- Scandinavian keyboard compensation
+map("n", "€", "$", { desc = "$ remap"})
