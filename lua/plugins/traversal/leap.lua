8fb24bacbc8a347bfbdcd215da58fe352e3bc0c4
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2024-01-07 20:14:16 +0100

    move leap to traversal folder

--- lua/plugins/traversal/leap.lua
+++ lua/plugins/traversal/leap.lua
@@ -7,7 +7,7 @@ return {
     enabled = true,
     lazy = false,
     keys = function()
-      ---@type LazyKeys[]
+      ---@type LazyKeysSpec[]
       local ret = {}
       for _, key in ipairs({ "f", "F", "t", "T" }) do
         ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
