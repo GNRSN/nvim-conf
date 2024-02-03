dc4543e6f7ddd019160f118acaa754e170722726
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2024-02-03 17:03:26 +0100

    add todo

--- lua/plugins/editing/visual-multi.lua
+++ lua/plugins/editing/visual-multi.lua
@@ -2,5 +2,9 @@ return {
   -- Multi cursor
   {
     "mg979/vim-visual-multi",
+    config = function()
+      -- LATER: Link to custom highlight group
+      -- see https://github.com/mg979/vim-visual-multi/blob/e67f7fa011c98fc5426352d3bb06362a0f70af3c/doc/vm-settings.txt#L201
+    end,
   },
 }
