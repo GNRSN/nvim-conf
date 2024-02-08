0208206a0ef48ef36ab6e0feaf34aa04a9a0253e
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2024-02-08 22:29:30 +0100

    try ultimate autopair

--- lua/plugins/editing/autopairs.lua
+++ lua/plugins/editing/autopairs.lua
@@ -1,31 +1,42 @@
 return {
-  "windwp/nvim-autopairs",
-  event = { "InsertEnter" },
-  dependencies = {
-    "hrsh7th/nvim-cmp",
-  },
-  opts = {
-    disable_filetype = { "TelescopePrompt", "vim" },
-  },
-  -- Interfers with visual multi D:
-  enabled = false,
-  config = function()
-    -- import nvim-autopairs
-    local autopairs = require("nvim-autopairs")
+  {
+    "windwp/nvim-autopairs",
+    -- Interfers with visual multi D:
+    enabled = false,
+    event = { "InsertEnter" },
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
+    opts = {
+      disable_filetype = { "TelescopePrompt", "vim" },
+    },
+    config = function()
+      -- import nvim-autopairs
+      local autopairs = require("nvim-autopairs")
 
-    -- configure autopairs
-    autopairs.setup({
-      check_ts = true, -- enable treesitter
-      ts_config = {
-        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-        javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-        java = false, -- don't check treesitter on java
-      },
-    })
+      -- configure autopairs
+      autopairs.setup({
+        check_ts = true, -- enable treesitter
+        ts_config = {
+          lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+          javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+          java = false, -- don't check treesitter on java
+        },
+      })
 
-    -- Insert parentheses after inserting function
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-    local cmp = require("cmp")
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  end,
+      -- Insert parentheses after inserting function
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
+  -- Alternative to nvim-autopairs, more features but maybe less stable
+  {
+    "altermo/ultimate-autopair.nvim",
+    event = { "InsertEnter", "CmdlineEnter" },
+    branch = "v0.6", --recommended as each new version will have breaking changes
+    opts = {
+      --Config goes here
+    },
+  },
 }
