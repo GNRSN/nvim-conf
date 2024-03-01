8598be46d38777140e35b6da0c9a2216429a33a4
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2024-03-01 15:56:26 +0100

    disable cursorword in neotest

--- lua/plugins/ui/cursor-word.lua
+++ lua/plugins/ui/cursor-word.lua
@@ -26,6 +26,7 @@ return {
           "Float",
           "Harpoon",
           "harpoon",
+          "neotest-summary",
         },
         -- filetypes_allowlist: filetypes to illuminate, this is overridden by filetypes_denylist
         -- You must set filetypes_denylist = {} to override the defaults to allow filetypes_allowlist to take effect
