af104324feb6c692f8cd839d76fe05dd9dd6d1b4
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-04-07 12:23:32 +0200

    try windows.nvim again

--- lua/plugins/nice-to-have/hydra.lua
+++ lua/plugins/nice-to-have/hydra.lua
@@ -26,20 +26,39 @@ return {
         "sindrets/winshift.nvim",
         config = true,
       },
-      -- {
-      --   -- "Zoom" splits, including animations
-      --   "anuvyklack/windows.nvim",
-      --   dependencies = {
-      --     "anuvyklack/middleclass",
-      --     "anuvyklack/animation.nvim",
-      --   },
-      --   config = function()
-      --     vim.o.winwidth = 10
-      --     vim.o.winminwidth = 10
-      --     vim.o.equalalways = false
-      --     require("windows").setup()
-      --   end,
-      -- },
+      {
+        -- "Zoom" splits, including animations
+        "anuvyklack/windows.nvim",
+        dependencies = {
+          "anuvyklack/middleclass",
+          "anuvyklack/animation.nvim",
+        },
+        event = "VeryLazy",
+        config = function()
+          vim.o.winwidth = 10
+          vim.o.winminwidth = 10
+          vim.o.equalalways = false
+          require("windows").setup({
+            ignore = {
+              buftype = {
+                "quickfix",
+              },
+              filetype = {
+                "neo-tree",
+                "neotest",
+                "neotest-summary",
+                "neotest-output-panel",
+                "Trouble",
+                "noice",
+                "undotree",
+                "spectre_panel",
+                "octo",
+                "DiffviewFiles",
+              },
+            },
+          })
+        end,
+      },
     },
     config = function()
       local Hydra = require("hydra")
