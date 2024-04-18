2809fb774b4dd953efa2db41ecc6246e4dd0f784
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-04-18 16:18:58 +0200

    move windows.nvim config to own file

--- lua/plugins/nice-to-have/hydra.lua
+++ lua/plugins/nice-to-have/hydra.lua
@@ -26,39 +26,7 @@ return {
         "sindrets/winshift.nvim",
         config = true,
       },
-      {
-        -- "Zoom" splits, including animations
-        "anuvyklack/windows.nvim",
-        dependencies = {
-          "anuvyklack/middleclass",
-          "anuvyklack/animation.nvim",
-        },
-        event = "VeryLazy",
-        config = function()
-          vim.o.winwidth = 10
-          vim.o.winminwidth = 10
-          vim.o.equalalways = false
-          require("windows").setup({
-            ignore = {
-              buftype = {
-                "quickfix",
-              },
-              filetype = {
-                "neo-tree",
-                "neotest",
-                "neotest-summary",
-                "neotest-output-panel",
-                "Trouble",
-                "noice",
-                "undotree",
-                "spectre_panel",
-                "octo",
-                "DiffviewFiles",
-              },
-            },
-          })
-        end,
-      },
+      "anuvyklack/windows.nvim",
     },
     config = function()
       local Hydra = require("hydra")
