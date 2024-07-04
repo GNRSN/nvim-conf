354f5ed83ba9186f7d23a2f958aa49b333293c65
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-07-04 21:51:03 +0200

    add keybinds for stepping between hunks

--- lua/plugins/ui/gitsigns.lua
+++ lua/plugins/ui/gitsigns.lua
@@ -2,7 +2,22 @@ return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
   keys = {
-    { "<leader>gb", "<cmd>Gitsigns blame_line<CR>" },
+    { "<leader>gb", "<cmd>Gitsigns blame_line<CR>", { desc = "Blame Line" } },
+    { "<leader>gB", "<cmd>Gitsigns blame<CR>", { desc = "Blame File" } },
+    {
+      "gn",
+      function()
+        require("gitsigns").next_hunk()
+      end,
+      { desc = "Next Hunk" },
+    },
+    {
+      "gp",
+      function()
+        require("gitsigns").prev_hunk()
+      end,
+      { desc = "Prev Hunk" },
+    },
   },
   opts = {
     -- From old conf
